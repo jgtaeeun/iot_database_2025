8bf08d5817f8ab4ec7dbbeef936b56f05cebaecb
jgtaeeun <jas49258667@gmail.com>
2025-03-06 09:00:08 +0900

    Update P07_셀프조인_coalesce.sql

--- SQL_practice/select_practice/P07_셀프조인_coalesce.sql
+++ SQL_practice/select_practice/P07_셀프조인_coalesce.sql
@@ -7,3 +7,13 @@ LEFT join ECOLI_DATA c
 ON p.ID = c.PARENT_ID
 group by p.ID
 ORDER by p.ID;
+
+-- 대장균 개체의 ID(ID)와 자식의 수(CHILD_COUNT)를 출력하는 SQL 문을 작성해주세요. 
+-- 자식이 없다면 자식의 수는 0으로 출력해주세요. 이때 결과는 개체의 ID 에 대해 오름차순 정렬해주세요.
+
+SELECT p.ID , COUNT(c.ID) AS CHILD_COUNT
+ FROM ECOLI_DATA p
+ LEFT JOIN ECOLI_DATA c
+ ON p.ID = c.PARENT_ID
+ GROUP BY p.ID
+ ORDER by p.ID;;
